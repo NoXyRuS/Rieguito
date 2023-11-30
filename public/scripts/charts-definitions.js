48108e11159c29d0a59895d7a3109421f04f85c7
NoXyRuS <bea_gonci@hotmail.es>
2023-11-30 11:01:35 -0500

    first commit

---
+++ public/scripts/charts-definitions.js
@@ -0,0 +1,104 @@
+window.addEventListener('load', onload);
+
+var chartS;
+
+function onload(event) {
+  chartS = createsensorChart();
+  //listenToFirebaseUpdates();
+}
+
+// Función para crear el gráfico de humedad
+function createsensorChart() {
+  var chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'chart-sensor',
+      type: 'spline'
+    },
+    series: [{
+      name: 'BME280'
+    }],
+    title: {
+      text: undefined
+    },
+    plotOptions: {
+      line: {
+        animation: false,
+        dataLabels: {
+          enabled: true
+        }
+      },
+      series: {
+        color: '#50b8b4'
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        formatter: function () {
+          // Formatear la etiqueta del eje X con día y hora
+          var date = new Date(this.value);
+          var day = date.toLocaleDateString(); // Día en formato local
+          var time = date.toLocaleTimeString(); // Hora en formato local
+          return day + '<br>' + time;
+        }
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'PORCENTAJE DE HUMEDAD (%)'
+      }
+    },
+    credits: {
+      enabled: false
+    }
+  });
+  return chart;
+}
+/*
+// Función para escuchar actualizaciones en Firebase
+function listenToFirebaseUpdates() {
+  var sensorRef = firebase.database().ref(databasePath + "/sensor");
+  var bombaRef = firebase.database().ref(databasePath + "/bomba");
+
+  // Escuchar cambios en el estado de la bomba
+  bombaRef.on('value', function (snapshot) {
+    var bomba = snapshot.val();
+    updateBombaState(bomba);
+  });
+
+  // Escuchar cambios en el porcentaje de humedad
+  sensorRef.on('value', function (snapshot) {
+    var ValorSensor = snapshot.val();
+    updatesensorChart(ValorSensor); // <-- Corregir el nombre de la función
+  });
+}
+
+// Función para actualizar el gráfico de humedad
+function updatesensorChart(value) {
+  var time = new Date().getTime();
+  chartS.series[0].addPoint([time, value]);
+  updatesensorValue(value);
+}
+
+// Función para actualizar el valor de humedad en el HTML
+function updatesensorValue(value) {
+  var sensorValueElement = document.getElementById("sensor");
+  sensorValueElement.textContent = value + " %";
+}
+
+// Función para actualizar el estado de la bomba en el HTML con color dinámico
+function updateBombaState(state) {
+  var bombaElement = document.getElementById("bomba");
+  var bombaIcon = bombaElement.querySelector("i.fa");
+  var bombaReading = bombaElement.querySelector(".reading");
+
+  // Actualizar el texto y color del ícono según el estado
+  if (state) {
+    bombaReading.textContent = "Encendida";
+    bombaIcon.style.color = "#19d600"; // Verde
+  } else {
+    bombaReading.textContent = "Apagada";
+    bombaIcon.style.color = "#e74c3c"; // Rojo
+  }
+}
+*/
---
+++ public/scripts/index.js
@@ -0,0 +1,114 @@
+
+const loginElement = document.querySelector('#login-form');
+const contentElement = document.querySelector("#content-sign-in");
+const userDetailsElement = document.querySelector('#user-details');
+const authBarElement = document.querySelector("#authentication-bar");
+
+// Elements for sensor readings
+const sensorElement = document.getElementById("sensor");
+const bombaElement = document.getElementById("bomba");
+
+var dbPathBomba;
+var dbRefSensor;  // Declare dbRefSensor outside the setupUI function
+
+
+// MANAGE LOGIN/LOGOUT UI
+const setupUI = (user) => {
+  if (user) {
+    //toggle UI elements
+    loginElement.style.display = 'none';
+    contentElement.style.display = 'block';
+    authBarElement.style.display ='block';
+    userDetailsElement.style.display ='block';
+    userDetailsElement.innerHTML = user.email;
+
+    // get user UID to get data from the database
+    var uid = user.uid;
+    console.log(uid);
+
+    // Database paths (with user UID)
+    var dbPathSensor = 'UsersData/' + uid.toString() + '/sensor';
+    dbPathBomba = 'UsersData/' + uid.toString() + '/bomba';
+
+    // Database references
+    dbRefSensor = firebase.database().ref().child(dbPathSensor);  // Initialize dbRefSensor
+
+    var dbRefBomba = firebase.database().ref().child(dbPathBomba);
+
+    // Update page with new readings
+    dbRefSensor.on('value', snap => {
+      sensorElement.innerText = snap.val();
+    });
+
+    dbRefBomba.on('value', snap => {
+      bombaElement.innerText = snap.val();
+    });
+
+    // Update chart with new sensor readings
+    dbRefSensor.on('value', snap => {
+      sensorElement.innerText = snap.val().toFixed(2);
+      var x = (new Date()).getTime();
+      var y = parseFloat(snap.val().toFixed(2));
+
+      if (chartS.series[0].data.length > 40) {
+        chartS.series[0].addPoint([x, y], true, true, true);
+      } else {
+        chartS.series[0].addPoint([x, y], true, false, true);
+      }
+    });
+
+    // Rest of the code... (additional adjustments may be needed based on the rest of the code)
+  } else {
+    // toggle UI elements
+    loginElement.style.display = 'block';
+    authBarElement.style.display ='none';
+    userDetailsElement.style.display ='none';
+    contentElement.style.display = 'none';
+  }
+}
+
+
+// Obtener el botón y agregar un manejador de eventos al hacer clic
+const generatePdfButton = document.getElementById('generate-pdf-button');
+generatePdfButton.addEventListener('click', () => {
+  // Lógica para generar el PDF
+  console.log("no funciona");
+  generatePDF();
+});
+// Función para formatear una hora a un formato legible (puedes personalizar esto según tus necesidades)
+function formatHour(hour) {
+  const options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  return new Date(hour).toLocaleTimeString('es-ES', options);
+}
+function formatDay(timestamp) {
+  const date = new Date(timestamp);
+  return Highcharts.dateFormat('%e %b', date);
+}
+
+// Función para generar el PDF
+function generatePDF() {
+  // Utiliza la biblioteca jsPDF para generar el PDF
+  console.log(window);
+  console.log(jspdf);
+  const pdf = new jspdf.jsPDF();
+  console.log("nola");
+  // Agrega contenido al PDF
+  pdf.text('Reporte de Información', 20, 10);
+  pdf.text(`Estado de la Bomba: ${bombaElement.innerText}`, 20, 20);
+  pdf.text(`Humedad de la Planta: ${sensorElement.innerText}`, 20, 30);
+  // Agrega datos del gráfico de humedad al PDF
+  pdf.text('Datos del Gráfico de Humedad:', 20, 40);
+
+  const chartData = chartS.series[0].data;
+
+  chartData.forEach((point, index) => {
+    const formattedHour = formatHour(point.x);
+    const formattedDay = formatDay(point.x);
+    pdf.text(`Día ${formattedDay}: Hora=${formattedHour}, Y=${point.y}`, 20, 50 + index * 10);
+  });
+
+  // Guarda el PDF (puedes ajustar el nombre del archivo según tus necesidades)
+  console.log("puedo");
+  pdf.save('reporte.pdf');
+  console.log("no puedo");
+}
